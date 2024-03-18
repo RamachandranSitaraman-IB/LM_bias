8aed2d2813e4a7e0a35ddf910a13702ae2469c02
RamachandranSitaraman-IB <v.sitaraman@infrablok.com>
2024-03-17 18:23:32 -0700

    change src to . in import

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -36,7 +36,7 @@ def heatmap(activations, layer_name):
     if isinstance(activations[layer_name], torch.Tensor):
         activations[layer_name] = activations[layer_name].detach()
     if isinstance(activations[layer_name], list) and not isinstance(activations[layer_name], torch.Tensor):
-        activations[layer_name] = np.array(activations[layer_name])
+        activations[layer_name] = activations[layer_name].detach().numpy()
     activation_tensor = activations[layer_name].squeeze(0)  # Remove batch dim if batch_size=1
 
     plt.figure(figsize=(10, 10))
